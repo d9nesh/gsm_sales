2d0a92c07e37b193b4220fef572b8bc8dec68562
d9nesh <dienshkr998@gmail.com>
2017-08-10 12:56:59 +0530

    Data tables and form

--- Server.js
+++ Server.js
@@ -16,9 +16,6 @@ connection.connect(function(err) {
   console.log("Connected!");
 });
 
-let select_profit_chart_asin = `CALL select_profit_chart_asin()`;
-let select_profit_chart_data = `CALL select_profit_chart_data('B01F1CP0DS,B01MT8HASA',10)`
-
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 hbs.registerPartials(__dirname + '/views/partials');
@@ -27,8 +24,9 @@ hbs.registerPartials(__dirname + '/views/partials');
 // app.set('views', './views');
 
 app.get('/', (req, res) => {
+  let select_profit_chart_asin = `CALL select_profit_chart_asin()`;
   connection.query(select_profit_chart_asin, true, (error, results, fields) => {
-    console.log(JSON.stringify(results, undefined, 2));
+    // console.log(JSON.stringify(results, undefined, 2));
     if (error) {
       return console.error(error.message);
     }
@@ -40,7 +38,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/chart', (req, res) => {
-  let select_profit_chart_data = `CALL select_profit_chart_data('B01F1CP0DS,B01MT8HASA',10)`
+  let chart = req.query['chart'];
+  let asinValues = req.query['asin-values'];
+  let asin = req.query['asin'];
+  let segment = req.query['segment'];
+  var select_profit_chart_data;
+  if (asin !== ""){
+    select_profit_chart_data = `CALL select_profit_chart_data("${asin}",10)`;
+  }else {
+    select_profit_chart_data = `CALL select_profit_chart_data("${asinValues}",10)`;
+  }
+  console.log(select_profit_chart_data);
+
+  // console.log('Business Segment: ' + segment);
+  // console.log('ASIN: ' + asin);
+  // console.log('Day to chart: ' + chart);
+
   connection.query(select_profit_chart_data, true, (error, results, fields) => {
     // console.log(JSON.stringify(results[0][0]['sum(quantity)'], undefined, 2));
     var data = results[0];
